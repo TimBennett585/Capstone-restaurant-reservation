7a74a9d70a6b7cf93633691ab2f31f8bc07bb5f6
Tim <Azacar02@gmail.com>
2024-01-31 22:31:55 -0500

    Same as last time

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -6,109 +6,108 @@ function ReservationForm({ handleSubmit, handleChange, reservation }) {
 
   return (
     <>
-            <form onSubmit={handleSubmit} className="justify-content-center">
-              <div className="form-group">
-                <label htmlFor="first_name" className="form-label">
-                  First Name
-                  <input
-                    type="text"
-                    id="first_name"
-                    name="first_name"
-                    placeholder="First Name"
-                    required
-                    onChange={handleChange}
-                    value={reservation.first_name}
-                    className="form-control"
-                  />
-                </label>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="last_name" className="form-label">
-                  Last Name
-                  <input
-                    type="text"
-                    id="last_name"
-                    name="last_name"
-                    placeholder="Last Name"
-                    required
-                    onChange={handleChange}
-                    value={reservation.last_name}
-                    className="form-control"
-                  />
-                </label>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="mobile_number" className="form-label">
-                  Mobile Number
-                  <input
-                    type="tel"
-                    id="mobile_number"
-                    name="mobile_number"
-                    placeholder="555-555-5555"
-                    onkeypress="return /[0-9-]/i.test(event.key)"
-                    required
-                    onChange={handleChange}
-                    value={reservation.mobile_number}
-                    className="form-control"
-                  />
-                </label>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="reservation_date" className="form-label">
-                  Reservation Date
-                  <input
-                    type="date"
-                    id="reservation_date"
-                    name="reservation_date"
-                    required
-                    placeholder="YYYY-MM-DD"
-                    onChange={handleChange}
-                    value={reservation.reservation_date}
-                    className="form-control"
-                  />
-                </label>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="reservation_time" className="form-label">
-                  Reservation Time
-                  <input
-                    type="time"
-                    id="reservation_time"
-                    name="reservation_time"
-                    placeholder="HH:MM:SS"
-                    required
-                    onChange={handleChange}
-                    value={reservation.reservation_time}
-                    className="form-control"
-                  />
-                </label>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="people" className="form-label">
-                  Party Size
-                  <input
-                    type="number"
-                    id="people"
-                    name="people"
-                    placeholder="2"
-                    required
-                    onChange={handleChange}
-                    value={reservation.people}
-                    className="form-control"
-                  />
-                </label>
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-              <button
-                type="button"
-                onClick={() => history.push("/")}
-                className="btn btn-secondary mx-1"
-              >
-                Cancel
-              </button>
-            </form>
+      <form onSubmit={handleSubmit} className="justify-content-center">
+        <div className="form-group">
+          <label htmlFor="first_name" className="form-label">
+            First Name
+            <input
+              type="text"
+              id="first_name"
+              name="first_name"
+              placeholder="First Name"
+              required
+              onChange={handleChange}
+              value={reservation.first_name}
+              className="form-control"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="last_name" className="form-label">
+            Last Name
+            <input
+              type="text"
+              id="last_name"
+              name="last_name"
+              placeholder="Last Name"
+              required
+              onChange={handleChange}
+              value={reservation.last_name}
+              className="form-control"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="mobile_number" className="form-label">
+            Mobile Number
+            <input
+              type="number"
+              id="mobile_number"
+              name="mobile_number"
+              placeholder="555-555-5555"
+              required
+              onChange={handleChange}
+              value={reservation.mobile_number}
+              className="form-control"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="reservation_date" className="form-label">
+            Reservation Date
+            <input
+              type="date"
+              id="reservation_date"
+              name="reservation_date"
+              required
+              placeholder="YYYY-MM-DD"
+              onChange={handleChange}
+              value={reservation.reservation_date}
+              className="form-control"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="reservation_time" className="form-label">
+            Reservation Time
+            <input
+              type="time"
+              id="reservation_time"
+              name="reservation_time"
+              placeholder="HH:MM:SS"
+              required
+              onChange={handleChange}
+              value={reservation.reservation_time}
+              className="form-control"
+            />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="people" className="form-label">
+            Party Size
+            <input
+              type="number"
+              id="people"
+              name="people"
+              placeholder="2"
+              required
+              onChange={handleChange}
+              value={reservation.people}
+              className="form-control"
+            />
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <button
+          type="button"
+          onClick={() => history.push("/")}
+          className="btn btn-secondary mx-1"
+        >
+          Cancel
+        </button>
+      </form>
     </>
   );
 }
